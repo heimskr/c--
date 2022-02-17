2b923acf5b25ca5cb24aef14cbda61be39dcfb45
Kai Tamkun <ktamkun@ucsc.edu>
2022-02-17 15:43:31 -0800

    Remove some debugspam

--- src/ColoringAllocator.cpp
+++ src/ColoringAllocator.cpp
@@ -13,84 +13,37 @@
 #include "Util.h"
 #include "Why.h"
 
-#define CONSTRUCT_BY_BLOCK
-#define SELECT_MOST_LIVE
-#define DEBUG_COLORING
-
 ColoringAllocator::Result ColoringAllocator::attempt() {
 	++attempts;
-#ifdef DEBUG_COLORING
-	std::cerr << "Allocating for \e[1m" << function.name << "\e[22m.\n";
-#endif
-
 	makeInterferenceGraph();
+
 	try {
 		interference.color(Graph::ColoringAlgorithm::Greedy, Why::temporaryOffset,
 			Why::savedOffset + Why::savedCount - 1);
 	} catch (const UncolorableError &err) {
-#ifdef DEBUG_COLORING
-		std::cerr << "Coloring failed.\n";
-#endif
 		VregPtr to_spill = selectMostLive();
-
-		if (!to_spill) {
-			error() << "to_spill is null!\n";
+		if (!to_spill)
 			throw std::runtime_error("to_spill is null");
-		}
-#ifdef DEBUG_COLORING
-		std::cerr << "Going to spill " << *to_spill;
-#if !defined(SELECT_LOWEST_COST) && !defined(SELECT_MOST_LIVE) && !defined(SELECT_CHAITIN)
-		std::cerr << " (degree: " << highest_degree << ")";
-#endif
-		std::cerr << ". Likely name: " << function.virtualRegisters.size() << '\n';
-		std::cerr << "Can spill: " << std::boolalpha << function.canSpill(to_spill) << '\n';
-#endif
 		triedIDs.insert(to_spill->id);
-#ifdef DEBUG_COLORING
-		info() << "Variable to spill: " << *to_spill << " (ID: " << to_spill->id << ")\n";
-#endif
 		lastSpillAttempt = to_spill;
 		triedIDs.insert(to_spill->id);
 
 		if (function.spill(to_spill)) {
-#ifdef DEBUG_COLORING
-			std::cerr << "Spilled " << *to_spill << ". Variables: " << function.virtualRegisters.size()
-			          << ". Stack usage: " << function.stackUsage << '\n';
-#endif
 			lastSpill = to_spill;
 			++spillCount;
-			int split = function.split();
-			if (0 < split) {
-#ifdef DEBUG_COLORING
-				std::cerr << split << " block" << (split == 1? " was" : "s were") << " split.\n";
-#endif
+			if (0 < function.split()) {
 				for (BasicBlockPtr &block: function.blocks)
 					block->cacheReadWritten();
 				function.computeLiveness();
 			}
-#ifdef DEBUG_COLORING
-			else std::cerr << "No blocks were split.\n";
-#endif
 			return Result::Spilled;
 		}
-#ifdef DEBUG_COLORING
-		else std::cerr << *to_spill << " wasn't spilled.\n";
-#endif
+
 		return Result::NotSpilled;
 	}
 
-#ifdef DEBUG_COLORING
-	std::cerr << "Spilling process complete. There " << (spillCount == 1? "was " : "were ") << spillCount << " "
-				<< "spill" << (spillCount == 1? ".\n" : "s.\n");
-#endif
-
 	for (const std::pair<const std::string, Node *> &pair: interference) {
 		VregPtr ptr = pair.second->get<VregPtr>();
-#ifdef DEBUG_COLORING
-		std::cerr << "Variable " << std::string(*ptr) << ": " << ptr->reg << " -> ( ";
-		for (const int color: pair.second->colors) std::cerr << color << ' ';
-		std::cerr << ")\n";
-#endif
 		if (ptr->reg == -1)
 			ptr->reg = *pair.second->colors.begin();
 	}
@@ -190,9 +143,6 @@ void ColoringAllocator::makeInterferenceGraph() {
 		}
 	}
 
-#ifdef DEBUG_COLORING
-	std::cerr << "Label count: " << labels.size() << '\n';
-#endif
 	for (const auto &[block_id, vec]: vecs) {
 		const size_t size = vec.size();
 		if (size < 2)
@@ -203,8 +153,4 @@ void ColoringAllocator::makeInterferenceGraph() {
 				++links;
 			}
 	}
-
-#ifdef DEBUG_COLORING
-	std::cerr << "Made " << links << " link" << (links == 1? "" : "s") << ".\n";
-#endif
 }
