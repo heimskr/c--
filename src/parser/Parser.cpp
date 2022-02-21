3c7fbee87a56abbbe3db83bf6f85657d4f852cb5
Kai Tamkun <ktamkun@ucsc.edu>
2022-02-21 01:35:09 -0800

    Split Parser properly

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -7,14 +7,20 @@
 #include "StringSet.h"
 
 int cmmwrap() { return 1; }
+int wasmwrap() { return 1; }
 
 extern YY_BUFFER_STATE cmm_scan_buffer(char *, size_t);
+extern YY_BUFFER_STATE wasm_scan_buffer(char *, size_t);
 extern void cmm_delete_buffer(YY_BUFFER_STATE);
+extern void wasm_delete_buffer(YY_BUFFER_STATE);
 
 void Parser::open(const std::string &filename_) {
 	errorCount = 0;
 	filename = filename_;
-	cmmin = fopen(filename.c_str(), "r");
+	if (type == Type::Cmm)
+		cmmin = fopen(filename.c_str(), "r");
+	else
+		wasmin = fopen(filename.c_str(), "r");
 }
 
 void Parser::in(const std::string &text) {
@@ -22,20 +28,34 @@ void Parser::in(const std::string &text) {
 	buffer = new char[text.size() + 2];
 	std::strncpy(buffer, text.c_str(), text.size() + 1);
 	buffer[text.size() + 1] = '\0'; // Input to flex needs two null terminators.
-	bufferState = cmm_scan_buffer(buffer, text.size() + 2);
+	if (type == Type::Cmm)
+		bufferState = cmm_scan_buffer(buffer, text.size() + 2);
+	else
+		bufferState = wasm_scan_buffer(buffer, text.size() + 2);
 }
 
 void Parser::debug(bool flex, bool bison) {
-	cmm_flex_debug = flex;
-	cmmdebug = bison;
+	if (type == Type::Cmm) {
+		cmm_flex_debug = flex;
+		cmmdebug = bison;
+	} else {
+		wasm_flex_debug = flex;
+		wasmdebug = bison;
+	}
 }
 
 void Parser::parse() {
-	cmmparse();
+	if (type == Type::Cmm)
+		cmmparse();
+	else
+		wasmparse();
 }
 
 void Parser::done() {
-	cmmlex_destroy();
+	if (type == Type::Cmm)
+		cmmlex_destroy();
+	else
+		wasmlex_destroy();
 
 	if (root) {
 		delete root;
